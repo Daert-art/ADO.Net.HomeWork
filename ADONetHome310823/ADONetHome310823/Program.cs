967ac41c24cb78d9568330d54b502d4c96c7e957
Daert-art <131146658+Daert-art@users.noreply.github.com>
2024-03-20 20:49:57 +0200

    ADO.Net_HW30082023

---
+++ ADONetHome310823/ADONetHome310823/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using System.Data;
+
+namespace ADONetHome310823
+{
+    internal class Program
+    {
+        private static string conectionString = "Data Source=DESKTOP-2SM04KA;Initial Catalog=VegetablesAndFruits;UID=Mark;PWD=123;";
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            using (SqlConnection connection = new SqlConnection(conectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine(conectionString);
+                connection.Close();
+            }
+
+        }
+    }
+}
